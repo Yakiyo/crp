9e2145ccbb27509edd7b6735a2bb30e1bc584cf5
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-11-12 11:02:08 +0600

    refactor: remove unnecessary sleep func

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use crp::input;
 use crp::load_conf;
 use crp::run;
 use std::process;
-use std::{thread, time};
 
 fn main() {
     // TODO: Check for new releases on running
@@ -17,8 +16,6 @@ fn main() {
             eprintln!("{e}");
             // This keeps the process running so the user can see the error output
             // instead of the terminal instantly closing
-            let t = time::Duration::from_secs(5);
-            thread::sleep(t);
             input("Press q/quit/exit to shutdown process:");
             process::exit(1);
         }
@@ -28,8 +25,6 @@ fn main() {
         eprintln!("Internal error, {e}");
         // This keeps the process running so the user can see the error output
         // instead of the terminal instantly closing
-        let t = time::Duration::from_secs(5);
-        thread::sleep(t);
         input("Press q/quit/exit to shutdown process:");
         process::exit(1);
     };
