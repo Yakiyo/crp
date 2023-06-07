5b0cfe5f0271736c96c16a7fafd6eaaf79db6794
Yakiyo <yakiyosama5855@gmail.com>
2023-06-07 15:31:32 +0000

    fix: still fails to find cargo command

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -7,9 +7,11 @@ apt-get install -y \
   jq \
   sudo \
   zsh \
+  bash \
   vim \
   build-essential \
-  openssl
+  openssl \
+  unzip
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
@@ -19,14 +21,14 @@ rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
 rustup component add clippy --toolchain nightly
 
+CARGO="~/.cargo/bin/cargo"
+
+CARGO install --locked bat
+CARGO install lsd
+
+echo alias bat="bat --pager=never" >> ~/.bashrc
+# Install oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+echo eval "$(oh-my-posh init bash)" >> ~/.bashrc
 exec bash
-
-cargo install cargo-expand
-cargo install cargo-edit
-
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
