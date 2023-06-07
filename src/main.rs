3d0f8c37ec89b4abd7fc49740915d461d479fd8b
Yakiyo <yakiyosama5855@gmail.com>
2023-06-07 15:48:17 +0000

    feat: restart everything

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,4 @@
-// CRP - Custom Rich Presence client for Discord
-//
-// Repository: https://github.com/Yakiyo/crp
-// Copyright 2022 Yakiyo. All rights reserved. MIT license.
-
-use crp::input;
-use crp::load_conf;
-use crp::run;
-use std::process;
 
 fn main() {
-    // TODO: Check for new releases on running
-    let config = match load_conf() {
-        Ok(conf) => conf,
-        Err(e) => {
-            eprintln!("{e}");
-            // This keeps the process running so the user can see the error output
-            // instead of the terminal instantly closing
-            input("Press q/quit/exit to shutdown process:");
-            process::exit(1);
-        }
-    };
-
-    if let Err(e) = run(&config) {
-        eprintln!("Internal error, {e}");
-        // This keeps the process running so the user can see the error output
-        // instead of the terminal instantly closing
-        input("Press q/quit/exit to shutdown process:");
-        process::exit(1);
-    };
+	println!("Hello World");
 }
