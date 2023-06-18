139e43e00a694bc74dee01bd96aae2485b6d4672
Yakiyo <yakiyosama5855@gmail.com>
2023-06-18 06:36:54 +0000

    chore: cargo fmt

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use crate::parser::Toml;
-use std::path::PathBuf;
 use anyhow::Context;
+use std::path::PathBuf;
 use yansi::Paint;
 
 pub const CONF_FILE: &str = "config.toml";
@@ -24,17 +24,17 @@ pub fn load_file() -> anyhow::Result<String> {
 }
 
 /// Config struct used in app
-/// 
+///
 /// This is flattened form of the `Toml` struct
 pub struct Config {
-	pub id: String,
-	pub state: String,
+    pub id: String,
+    pub state: String,
     pub details: String,
     pub start_timestamp: Option<String>,
     pub end_timestamp: Option<String>,
-	pub l_img: Option<(String, String)>,
+    pub l_img: Option<(String, String)>,
     pub s_img: Option<(String, String)>,
-	pub first_label: Option<String>,
+    pub first_label: Option<String>,
     pub first_url: Option<String>,
     pub second_label: Option<String>,
     pub second_url: Option<String>,
@@ -42,16 +42,22 @@ pub struct Config {
 
 impl std::convert::From<Toml> for Config {
     fn from(v: Toml) -> Self {
-		let l_img = if v.images.large_image.is_none() {
-			None
-		} else {
-			Some((v.images.large_image.unwrap(), v.images.large_image_tooltip.unwrap_or_default()))
-		};
-		let s_img = if v.images.small_image.is_none() {
-			None
-		} else {
-			Some((v.images.small_image.unwrap(), v.images.small_image_tooltip.unwrap_or_default()))
-		};
+        let l_img = if v.images.large_image.is_none() {
+            None
+        } else {
+            Some((
+                v.images.large_image.unwrap(),
+                v.images.large_image_tooltip.unwrap_or_default(),
+            ))
+        };
+        let s_img = if v.images.small_image.is_none() {
+            None
+        } else {
+            Some((
+                v.images.small_image.unwrap(),
+                v.images.small_image_tooltip.unwrap_or_default(),
+            ))
+        };
         Config {
             id: v.id,
             state: v.state.state,
@@ -61,9 +67,9 @@ impl std::convert::From<Toml> for Config {
             l_img,
             s_img,
             first_label: v.buttons.first_label,
-			first_url: v.buttons.first_url,
-			second_label: v.buttons.second_label,
-			second_url: v.buttons.second_url,
+            first_url: v.buttons.first_url,
+            second_label: v.buttons.second_label,
+            second_url: v.buttons.second_url,
         }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -45,24 +45,27 @@ pub struct Buttons {
 
 /// Parse toml string to config struct
 pub fn parse<S: AsRef<str>>(s: S) -> anyhow::Result<&'static mut Toml> {
-    let toml = toml::from_str::<Toml>(s.as_ref()).context("Invalid config file")?.validate().context("Invalid config file");
-	match toml {
-		Ok(e) => Ok(e),
-		Err(e) => Err(e),
-	}
+    let toml = toml::from_str::<Toml>(s.as_ref())
+        .context("Invalid config file")?
+        .validate()
+        .context("Invalid config file");
+    match toml {
+        Ok(e) => Ok(e),
+        Err(e) => Err(e),
+    }
 }
 
 impl Toml {
     fn validate(&mut self) -> anyhow::Result<&mut Toml> {
-		if self.id.chars().all(|x| x.is_ascii_digit()) && self.id.chars().count() < 17 {
-			bail!("ID parameter must only contain digits and be longer than 17 digits")
-		}
-		if self.images.large_image.is_some_and(|x| x.is_empty()) {
-			self.images.large_image = None;
-		}
-		if self.images.small_image.is_some_and(|x| x.is_empty()) {
-			self.images.small_image = None;
-		}
+        if self.id.chars().all(|x| x.is_ascii_digit()) && self.id.chars().count() < 17 {
+            bail!("ID parameter must only contain digits and be longer than 17 digits")
+        }
+        if self.images.large_image.is_some_and(|x| x.is_empty()) {
+            self.images.large_image = None;
+        }
+        if self.images.small_image.is_some_and(|x| x.is_empty()) {
+            self.images.small_image = None;
+        }
         Ok(self)
     }
 }
